5c8c409405a023ebf83771f8f1ec9832c7872970
divin-mihigo <d.mihigo@alustudent.com>
2026-09-22 15:26:42 +0200

    Update hospital_admin.sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -18,9 +18,3 @@ initialize_system() {
     echo "System initialization complete."
 }
 initialize_system
-
-secure_data() {
-    chmod 700 active_logs
-    echo "New permissions for active_logs:"
-    ls -ld active_logs
-}
